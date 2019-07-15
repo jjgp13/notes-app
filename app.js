66720d8716f23de6c9817ae2cf6dd104410087ac
Jose Galvez <jjgalper@gmail.com>
2019-07-15 15:06:11 -0500

    Installing modules and practice with them

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const fs = require('fs');
+// const add = require('./utils.js');
+// console.log(add(5,3));
+//const validator = require('validator');
+const chalk = require('chalk');
+const notes = require('./notes.js');
+console.log(notes());
 
-fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+// console.log(validator.isEmail('jjgalpergmail.com'));
+// console.log(validator.isURL('https:/mead.io'));
+
+console.log(chalk.bold.green.inverse('Success!'));
+console.log(chalk.bold.red.inverse('Danger!'));
